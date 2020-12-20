a845846f486b24887d4052bf0f7af0e7181cff78
mohamedsaid000 <mohamed.said.ai098@gmail.com>
2020-12-20 06:29:51 -0600

    adding index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php 
+//if button upload has been clicked
+if(isset($_POST['submit'])){
+ 
+    // Count total files
+    $countfiles = count($_FILES['file']['name']);
+
+    // Looping all files 
+    for($i=0;$i<$countfiles;$i++){
+       $filename = $_FILES['file']['name'][$i];
+        // Upload file
+        move_uploaded_file($_FILES['file']['tmp_name'][$i],'upload/'.$filename);
+
+    
+    }
+
+} 
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- jquery cdn -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <title>Multiple File Upload</title>
+  </head>
+  <body>
+    <br>
+    <br>
+    <br>
+    <form method='post' action='' enctype='multipart/form-data'>
+        <div class="container">
+            <div class="custom-file">
+                <input type="file" name="file[]" class="custom-file-input" id="customFile" multiple>
+                <label class="custom-file-label" for="customFile">Choose file</label>
+            </div>
+            <br>
+            <button type="submit" name="submit" class="btn btn-primary">Upload</button>
+        </div>
+    </form>
+
+    <script>
+        // Add the following code if you want the name of the file appear on select
+        $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+        });
+    </script>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
